477ef1a2817dd79f69eac7f688d666c20ae25b19
Yeet-Nicky <nickyheldoorn@gmail.com>
2025-02-17 14:04:40 +0100

    auto

--- Programma/weatherhat1.py
+++ Programma/weatherhat1.py
@@ -2,6 +2,7 @@ import weatherhat
 import time
 import csv
 from datetime import datetime
+import os
 
 sensor = weatherhat.WeatherHAT()
 
@@ -12,35 +13,42 @@ with open('weather_data.csv', mode='w', newline='') as file:
                      "Windsnelheid (m/s)", "Windrichting (graden)"])
 
 while True:
- apparaat_temperatuur = sensor.device_temperature 
- tempratuur = sensor.temperature
- druk = sensor.pressure
- vochtigheid = sensor.humidity
- lux = sensor.lux
- windkracht = sensor.wind_speed
- windrichting = sensor.wind_direction
- 
- 
- print(f"Apparaat temperatuur: {apparaat_temperatuur:.2f} °C")
- 
- print(f"temperatuur: {tempratuur:.2f}")
- 
- print(f"luchtdruk: {druk:.2f} hPa")
- 
- print(f"Luchtvochtigheid: {vochtigheid:.2f} %")
- 
- print(f"Lichtintensiteit: {lux:.2f} lux")
- 
- print(f"Windsnelheid: {windkracht:.2f} m/s")
- 
- print(f"Windrichting: {windrichting:.2f} graden")
- 
- 
- with open('weather_data.csv', mode='a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([datetime.now(), apparaat_temperatuur, tempratuur, 
-                         druk, vochtigheid, lux, windkracht, windrichting])
- 
- sensor.update(interval=5)
- time.sleep(10)
- 
+    apparaat_temperatuur = sensor.device_temperature 
+    tempratuur = sensor.temperature
+    druk = sensor.pressure
+    vochtigheid = sensor.humidity
+    lux = sensor.lux
+    windkracht = sensor.wind_speed
+    windrichting = sensor.wind_direction
+    
+    
+    print(f"Apparaat temperatuur: {apparaat_temperatuur:.2f} °C")
+    
+    print(f"temperatuur: {tempratuur:.2f}")
+    
+    print(f"luchtdruk: {druk:.2f} hPa")
+    
+    print(f"Luchtvochtigheid: {vochtigheid:.2f} %")
+    
+    print(f"Lichtintensiteit: {lux:.2f} lux")
+    
+    print(f"Windsnelheid: {windkracht:.2f} m/s")
+    
+    print(f"Windrichting: {windrichting:.2f} graden")
+    
+    
+    with open('weather_data.csv', mode='a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([datetime.now(), apparaat_temperatuur, tempratuur, 
+                            druk, vochtigheid, lux, windkracht, windrichting])
+
+    repo_path = "/home/rpi/weatherhat-python/ICT-projecten/Programma"
+    os.chdir(repo_path)
+
+    os.system("git add .")
+    os.system(f'git commit -m "auto"')
+    os.system("git push origin main")
+
+    sensor.update(interval=5)
+    time.sleep(10)
+    
